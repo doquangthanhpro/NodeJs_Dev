9a6a40c3bda353323e41f2064452196b6629453f
doquangthanhpro <minhhacong912@gmail.com>
2023-03-07 11:05:00 +0700

    update code

--- src/index.js
+++ src/index.js
@@ -1,11 +1,27 @@
-const express = require('express')
+const express = require("express");
 const app = express();
- const port = 3000
- 
- // template engine
+const path = require('path')
+const {engine} = require("express-handlebars");
+// config public folder
+ app.use(express.static(path.join(__dirname , 'public')))
+// config dotenv
+const dotenv = require("dotenv");
+dotenv.config();
+const port = process.env.PORT || 3000;
+// http logger midlewear
+const morgan = require("morgan");
+app.use(morgan("combined"));
+//template engine
+app.engine('hbs', engine({extname: '.hbs'}));
+app.set("view engine", "hbs");
+app.set('views', '/views');
+ console.log("dirname",__dirname)
+ app.set('views',path.join(__dirname , 'resources/views'))
 app.get("/", (req, res) => {
-  res.send("hello world");
+  res.render('home')
+
 });
- app.listen(port, () => {
-    console.log(`sever to connect http://localhost:${port}`); 
- }) 
+
+app.listen(port, () => {
+  console.log(`sever to connect http://localhost:${port}`);
+});
